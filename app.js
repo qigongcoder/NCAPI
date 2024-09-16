fb281eec136e8ae588c1a12a4c6efa57f64910ff
qigongcoder <nick@qigongcoder.com>
2024-09-16 10:48:14 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,62 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
 const {
-    getTopics,
-    getEndPoints,
-    getArticleById,
-    getArticles,
-	getArticleComments,
-	postComment,
-	patchVoteCount,
-	deleteCommentById,
-	getUsers,
+  getTopics,
+  getEndPoints,
+  getArticleById,
+  getArticles,
+  getArticleComments,
+  postComment,
+  patchVoteCount,
+  deleteCommentById,
+  getUsers,
 } = require("./controllers/news-controllers");
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get('/api/topics',getTopics);
+app.get("/api/topics", getTopics);
 
-app.get('/api/',getEndPoints);
+app.get("/api/", getEndPoints);
 
-app.get('/api/article/:article_id',getArticleById);
+app.get("/api/article/:article_id", getArticleById);
 
-app.get('/api/articles',getArticles)
+app.get("/api/articles", getArticles);
 
-app.get('/api/articles/:article_id/comments',getArticleComments)
+app.get("/api/articles/:article_id/comments", getArticleComments);
 
-app.post('/api/articles/:article_id/comments',postComment)
+app.post("/api/articles/:article_id/comments", postComment);
 
-app.patch('/api/articles/:article_id',patchVoteCount)
+app.patch("/api/articles/:article_id", patchVoteCount);
 
-app.delete('/api/comments/:comment_id', deleteCommentById);
+app.delete("/api/comments/:comment_id", deleteCommentById);
 
-app.get('/api/users',getUsers);
+app.get("/api/users", getUsers);
 
-
-
-
-app.use((error, request, response, next)=>{
-	if(error.status && error.message){
-		response.status(error.status).send({message: error.message});
-	}else{
-		next(error);
-	}
+app.use((error, request, response, next) => {
+  if (error.status && error.message) {
+    response.status(error.status).send({ message: error.message });
+  } else {
+    next(error);
+  }
 });
 
-app.use((error, request, response, next)=>{
-	if(error.code==="22P02"){
-		response.status(400).send({message: "BAD REQUEST"});
-	}else{
-		next(error);
-	}
+app.use((error, request, response, next) => {
+  if (error.code === "22P02") {
+    response.status(400).send({ message: "BAD REQUEST" });
+  } else {
+    next(error);
+  }
 });
 
-app.use((error, request, response, next)=>{
-	if(error.code==="23502"){
-		response.status(400).send({message: "BAD REQUEST"})
-	}
-})
-
-
+app.use((error, request, response, next) => {
+  if (error.code === "23502") {
+    response.status(400).send({ message: "BAD REQUEST" });
+  }
+});
 
 module.exports = app;
